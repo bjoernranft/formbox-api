da3c7ea2167bdfad03225f2c460be1c4ce0bf909
Eymux <eymux2009@gmail.com>
2017-11-24 16:03:26 +0100

    Grundlage für die Config API.

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -3,30 +3,43 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Router } from 'express-serve-static-core';
 import { Logger } from 'ts-log-debug';
+import * as path from 'path';
 
 @Injectable()
 export class AppMain {
   private app: express.Application;
   private log: Logger;
 
+  private db: Router;
+  private config: Router;
+
   constructor( @Inject('Logger') log: Logger,
     @Inject('Application') app: express.Application,
-    @Inject('DatabaseApi') db: Router) {
+    @Inject('DatabaseApi') db: Router,
+    @Inject('ConfigurationApi') config: Router) {
+
+    this.db = db;
+    this.config = config;
+
     this.log = log;
     this.log.debug('AppMain init.');
     this.app = app;
-    this.startServer(db);
+    this.startServer();
 
   }
 
-  startServer(db: Router) {
+  startServer() {
     this.log.debug('Starte Server.');
 
     this.app.set('port', process.env.PORT || 4201);
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
 
-    this.app.use('/db', db);
+    this.app.use('/db', this.db);
+    this.app.use('/config', this.config);
+
+    this.app.use('/assets/fragmente', express.static(path.join(process.env.ASSETS, '/fragmente')));
+    this.app.use('/assets/vorlagen', express.static(path.join(process.env.ASSETS, '/vorlagen')));
 
     this.app.listen(this.app.get('port'), () => {
       this.log.info(('App is running at http://localhost:%d.'), this.app.get('port'));
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import * as express from 'express';
 
 import { AppMain } from './app/app.main';
 import { DatabaseRouter } from './api/database.api';
+import { ConfigurationRouter } from './api/configuration.api';
+import { ConfigurationService } from './services/configuration.service';
+
+console.log(__dirname);
 
 dotenv.config();
 
@@ -33,9 +37,11 @@ log.appenders
 // die injiziert werden sollen.
 const injector = ReflectiveInjector.resolveAndCreate([
   AppMain,
+  ConfigurationService,
   { provide: 'Logger', useValue: log },
   { provide: 'Application', useValue: app },
   { provide: 'DatabaseApi', useFactory: DatabaseRouter, deps: [ 'Logger' ] },
+  { provide: 'ConfigurationApi', useFactory: ConfigurationRouter, deps: [ 'Logger', ConfigurationService ] },
 ]);
 
 // Startet die Anwendung über Dependency Injection.
