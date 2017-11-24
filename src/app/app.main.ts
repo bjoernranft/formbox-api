466d6bec795d49d6b98f677cec1779c161643e9c
Eymux <eymux2009@gmail.com>
2017-11-24 10:15:02 +0100

    Express Application wird jetzt injected.

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -1,21 +1,21 @@
-import { Injectable } from "injection-js";
-import * as express from "express";
-import * as bodyParser from "body-parser";
+import { Injectable, Inject } from 'injection-js';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 @Injectable()
 export class AppMain {
   private app: express.Application;
 
-  constructor() {
-    console.log("AppMain init.");
-    this.app = express();
+  constructor( @Inject('Application') app: express.Application) {
+    console.log('AppMain init.');
+    this.app = app;
 
-    this.app.set("port", 4201);
+    this.app.set('port', 4201);
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
 
-    this.app.listen(this.app.get("port"), () => {
-      console.log(("App is running at http://localhost:%d."), this.app.get("port"));
+    this.app.listen(this.app.get('port'), () => {
+      console.log(('App is running at http://localhost:%d.'), this.app.get('port'));
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
-import { ReflectiveInjector } from "injection-js";
-import { AppMain } from "./app/app.main";
+import { ReflectiveInjector } from 'injection-js';
+import * as express from 'express';
+
+import { AppMain } from './app/app.main';
+
+const app = express();
 
 // Hier müssen alle Klassen eingetragen werden,
 // die injiziert werden sollen.
 const injector = ReflectiveInjector.resolveAndCreate([
-  AppMain
+  AppMain,
+  { provide: 'Application', useValue: app }
 ]);
 
 // Startet die Anwendung über Dependency Injection.
