daf48e15d2167248338192da0ffa797dc14dbb34
bjoernranft <externer.dl.ranft@muenchen.de>
2017-12-11 13:08:32 +0100

    Codeformatierung, Pfad zu Zertifikaten ueber Umgebungskonfiguration moeglich

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -1,8 +1,8 @@
-import { Injectable, Inject } from 'injection-js';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { Router } from 'express-serve-static-core';
 import { Logger } from 'ts-log-debug';
+import { Router } from 'express-serve-static-core';
+import { Inject, Injectable } from 'injection-js';
 import * as path from 'path';
 import * as filesystem from 'fs';
 import * as https from 'https';
@@ -24,31 +24,31 @@ export class AppMain {
     this.app = app;
     this.log = log;
     this.log.debug('AppMain init.');
-    
+
     this.configServer();
     this.startServer();
   }
-  
-  configServer() {
-      this.app.set('port', process.env.PORT || 4201);
-      this.app.use(bodyParser.json());
-      this.app.use(bodyParser.urlencoded({ extended: true }));
 
-      this.app.use('/db', this.db);
-      this.app.use('/config', this.config);
+  configServer(): void {
+    this.app.set('port', process.env.PORT || 4201);
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
 
-      this.app.use('/assets/fragmente', express.static(path.join(process.env.ASSETS, '/fragmente')));
-      this.app.use('/assets/vorlagen', express.static(path.join(process.env.ASSETS, '/vorlagen')));
+    this.app.use('/db', this.db);
+    this.app.use('/config', this.config);
+
+    this.app.use('/assets/fragmente', express.static(path.join(process.env.ASSETS, '/fragmente')));
+    this.app.use('/assets/vorlagen', express.static(path.join(process.env.ASSETS, '/vorlagen')));
   }
 
-  startServer() {
+  startServer(): void {
     this.log.debug('Starte Server.');
-    const privateKey = filesystem.readFileSync('certs/server.key', 'utf8');
-    const certificate = filesystem.readFileSync('certs/server.crt', 'utf8');
+    const privateKey = filesystem.readFileSync(process.env.CERT_KEY_PATH, 'utf8');
+    const certificate = filesystem.readFileSync(process.env.CERT_CRT_PATH, 'utf8');
     const credentials = { key: privateKey, cert: certificate };
     const httpsServer = https.createServer(credentials, this.app);
     httpsServer.listen(this.app.get('port'), () => {
-        this.log.info(('App is running at https://localhost:%d.'), this.app.get('port'));
+      this.log.info(('App is running at https://localhost:%d.'), this.app.get('port'));
     });
   }
 }
