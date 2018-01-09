9627b5e5b84bd43674241079effbeb52744ad85e
bjoernranft <externer.dl.ranft@muenchen.de>
2018-01-09 01:58:53 +0100

    Asynchrone Routen

--- src/api/configuration.api.ts
+++ src/api/configuration.api.ts
@@ -2,22 +2,23 @@ import * as express from 'express';
 import { Router } from 'express-serve-static-core';
 import { Logger } from 'ts-log-debug';
 import { ConfigurationService } from '../services/configuration.service';
+import * as asyncHandler from 'express-async-handler';
 
 export function ConfigurationRouter(log: Logger, config: ConfigurationService): Router {
   log.debug('Initialisiere Configuration API.');
   const api = express.Router();
 
-  api.get('/', (req, res) => {
+  api.get('/', asyncHandler((req, res) => {
     res.json({ message: 'FormBox Configuration API' });
-  });
+  }));
 
-  api.get('/fragmente', (req, res) => {
+  api.get('/fragmente', asyncHandler((req, res) => {
     const name = req.query.name;
 
     if (name) {
       res.json({ path: config.getFragment(name) });
     }
-  });
+  }));
 
   return api;
 }
--- src/api/database.api.ts
+++ src/api/database.api.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
 import { Router } from 'express-serve-static-core';
 import { Logger } from 'ts-log-debug';
+import * as asyncHandler from 'express-async-handler';
 
 export function DatabaseRouter(log: Logger): Router {
   log.debug('Initialisiere Database API.');
   const api = express.Router();
 
-  api.get('/', (req, res) => {
+  api.get('/', asyncHandler( (req, res, next) => {
     res.json({ message: 'FormBox Databse API' });
-  });
-
+  }));
+  
   return api;
 }
